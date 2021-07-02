f5688a118ff05a8db5da5c3fe563e7933f02e476
Gilang R. Aprianto <gizipp.gizipp@gmail.com>
2021-07-03 05:27:25 +0700

    Request to Styvio stocks now available as lib.

---
+++ lib/stocks/styvio.rb
@@ -0,0 +1,27 @@
+module Stocks
+  class Styvio
+    def get(ticker)
+      send_request!(
+        URI("https://www.styvio.com/api/#{ticker}")
+      )
+    end
+
+    def sentiment(ticker)
+      send_request!(
+        URI("https://www.styvio.com/api/sentiment/#{ticker}")
+      )
+    end
+
+    private
+      def send_request!(url)
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+
+        request = Net::HTTP::Get.new(url)
+        request["Content-Type"] = "application/json"
+
+        response = https.request(request)
+        JSON.parse(response.read_body)
+      end
+  end
+end
